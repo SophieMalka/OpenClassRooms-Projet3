3de9a4ca6c20795c52b3a9d1f7ca9bfa7e8c6b70
SophieMalka <s.malka@live.fr>
2023-01-24 09:10:28 +0100

    Ajout du style via JS

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,5 +1,23 @@
 /**
- * Fonction afficher les projets
+ * 
+ * @param {String} param Suppression de tous les projets dans la div Gallery
+ */
+function deleteWorks (param) {
+    console.log(param);
+    //récupération de l'élément avec la classe CSS gallery (Objet HTMLcollection)
+ const gallery = document.getElementsByClassName("gallery").item(0);
+ console.log(gallery);
+
+ //suppression de tous les enfants de cet élément (boucle WHILE)
+ //alert("Purge !");
+ while (gallery.hasChildNodes()) {
+     gallery.removeChild(gallery.firstChild);
+ }
+};
+ 
+/**
+ * 
+ * @param {String} catId Ajout de tous les projets dans la div Gallery
  */
 function displayWorks(catId) {
 
@@ -23,12 +41,16 @@ function displayWorks(catId) {
     console.log(gallery);
 
     //ajout des works issus de l'API (boucle sur les données de l'API)
-    //alert("Alimentation !");
     //pour chaque donnée de l'API...
     setWorks(data, catId);
     });
 }
 
+/**
+ * 
+ * @param {String} data Récupération des projets sur l'API
+ * @param {String} catId Filtrage des projets en fonction de leur catégorie
+ */
 function setWorks (data, catId) {
     console.log(catId);
 
@@ -63,6 +85,7 @@ function setWorks (data, catId) {
     }
 };
 
+
 // récupération des catégories sur l'API
 fetch("http://localhost:5678/api/categories")
     .then(function(response) {
@@ -83,12 +106,38 @@ fetch("http://localhost:5678/api/categories")
     //on crée un noeud de type ul avec une classe filtres
     let divFiltres = document.createElement("div");
     divFiltres.classList.add("filtres");
+    divFiltres.style.display = 'flex';
+    divFiltres.style.flexDirection = 'row';
+    divFiltres.style.justifyContent = 'center';
+    divFiltres.style.columnGap = '10px';
+    divFiltres.style.marginBottom = '50px';
 
     //on crée le filtre "Tous"
     let buttonTous = document.createElement("button");
     buttonTous.innerText = "Tous";
     buttonTous.setAttribute('id', 'btn-filtre-0');
     buttonTous.value = 0;
+    buttonTous.addEventListener("click", function(event){
+    });
+    buttonTous.style.fontFamily = 'Syne';
+    buttonTous.style.fontSize = '16px';
+    buttonTous.style.fontWeight = '700';
+    buttonTous.style.lineHeight = '19px';
+    buttonTous.style.color = '#1D6154';
+    buttonTous.style.border = '1px solid #1D6154';
+    buttonTous.style.backgroundColor = '#FFFFFF';
+    buttonTous.style.borderRadius = '60px';
+    buttonTous.style.width = 'fit-content';
+    buttonTous.style.padding = '8px 20px';
+    buttonTous.addEventListener("mouseover", function(event){
+        event.target.style.textDecoration = 'none';
+        event.target.style.color = '#FFFFFF';
+        event.target.style.backgroundColor = '#1D6154';
+    });
+    buttonTous.addEventListener("mouseout", function(event){
+        event.target.style.color = '#1D6154';
+        event.target.style.backgroundColor = '#FFFFFF';
+    });
 
     //on met en place la structure DOM de l'élément crée ci-dessus
     divFiltres.appendChild(buttonTous);
@@ -108,7 +157,28 @@ fetch("http://localhost:5678/api/categories")
             deleteWorks("click");
             displayWorks(event.target.value);
         });
-        
+        // on ajoute le style aux boutons de filtre
+        newButton.style.fontFamily = 'Syne';
+        newButton.style.fontSize = '16px';
+        newButton.style.fontWeight = '700';
+        newButton.style.lineHeight = '19px';
+        newButton.style.color = '#1D6154';
+        newButton.style.border = '1px solid #1D6154';
+        newButton.style.backgroundColor = '#FFFFFF';
+        newButton.style.borderRadius = '60px';
+        newButton.style.width = 'fit-content';
+        newButton.style.padding = '8px 20px';
+        // on ajoute le style aux bouton de filtre lors du survol
+        newButton.addEventListener("mouseover", function(event){
+            event.target.style.textDecoration = 'none';
+            event.target.style.color = '#FFFFFF';
+            event.target.style.backgroundColor = '#1D6154';
+        });
+        newButton.addEventListener("mouseout", function(event){
+            event.target.style.color = '#1D6154';
+            event.target.style.backgroundColor = '#FFFFFF';
+        });
+
         // on met en place la structure DOM des différentes balises crées ci dessus
         divFiltres.appendChild(newButton);
     };
@@ -118,46 +188,4 @@ fetch("http://localhost:5678/api/categories")
 
     // on ajoute cet élement au DOM dans la section portfolio
     portfolio.insertBefore(divFiltres, gallery);
-
-    // Création de la fonction filtrer sur les boutons
-
-    // On récupère les données work de l'API
-    // récupération des projets sur l'API
-    fetch("http://localhost:5678/api/works")
-    .then(function(response) {
-        console.log(response);
-        if(response.ok) {
-            return response.json();
-    }
-    })
-
-    .then(function(data) {
-        console.log(data);
-
-        const btnFilterObjet = document.getElementById("btn-filtre-1");
-        console.log(btnFilterObjet);
-
-        for (let work of data) {
-            let workId = work.categoryId;
-            console.log(workId);
-        };
-    });
 });
-
-/**
- * 
- * @param {String} param okok
- */
-function deleteWorks (param) {
-    console.log(param);
-    //récupération de l'élément avec la classe CSS gallery (Objet HTMLcollection)
- const gallery = document.getElementsByClassName("gallery").item(0);
- console.log(gallery);
-
- //suppression de tous les enfants de cet élément (boucle WHILE)
- //alert("Purge !");
- while (gallery.hasChildNodes()) {
-     gallery.removeChild(gallery.firstChild);
- }
-};
- 
