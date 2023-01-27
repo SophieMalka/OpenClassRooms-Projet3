d7064bcb543f3bc62cfa658e786a26b77c640477
SophieMalka <s.malka@live.fr>
2023-01-27 11:25:16 +0100

    Ajout commentaire

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,12 +1,19 @@
+//Récupération de l'élément form
 const form = document.querySelector("form");
 
+/**
+ * 
+ * @param {string} event connexion 
+ */
 async function onSubmit(event) {
     event.preventDefault();
+    // définition de l'utilisation
     let user = {
         email: form.email.value,
         password: form.password.value,
     };
     
+    // récupération des données API
     let response = await fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: {
@@ -17,9 +24,11 @@ async function onSubmit(event) {
     
     let result = await response.json();
 
+    // si les identifiants sont justes
     if (response.status === 200) {
         localStorage.setItem("token", result.token);
         window.location.replace(`index.html`);
+    // sinon, si les identifiants sont faux
     } else if (response.status === 404 || response.status === 401) {
         form.email.value = "";
         form.password.value = "";
