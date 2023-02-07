f49f31742f855874015b33ad38da3084b53ef80a
SophieMalka <s.malka@live.fr>
2023-02-07 11:13:15 +0100

    Visuel modale ok

--- FrontEnd/js/modale.js
+++ FrontEnd/js/modale.js
@@ -24,3 +24,55 @@ document.querySelectorAll('.js-modal').forEach(a => {
     a.addEventListener('click', openModal);
 });
 
+function displayWorksModal() {
+    fetch("http://localhost:5678/api/works")
+    .then(function(response) {
+        console.log(response);
+        if(response.ok) {
+            return response.json();
+    }
+    })
+
+    .then(function(data) {
+    // une fois qu'on a les données de l'API
+    console.log(data);
+
+    const modalGallery = document.getElementById('modal-gallery');
+    console.log(modalGallery);
+
+    //ajout des works issus de l'API (boucle sur les données de l'API)
+    //pour chaque donnée de l'API...
+    setWorksModal(data);
+    });
+
+};
+
+function setWorksModal(data) {
+
+    for (let work of data) {
+        let newFigure = document.createElement("figure");
+        newFigure.style.width = '85px';
+        newFigure.style.height = '128px';
+
+        let newImg = document.createElement("img");
+        newImg.setAttribute("crossorigin", "anonymous");
+        newImg.setAttribute("src", work.imageUrl);
+        newImg.alt = work.title;
+        newImg.style.width = '100%';
+        newImg.style.objectFit = 'cover';
+
+        let newCaption = document.createElement("figcaption");
+        newCaption.innerText = 'éditer';
+
+        newFigure.appendChild(newImg);
+        newFigure.appendChild(newCaption);
+
+        const modalGallery = document.getElementById('modal-gallery');
+        console.log(modalGallery);
+
+        modalGallery.appendChild(newFigure);
+    }
+};
+
+displayWorksModal();
+setWorksModal();
