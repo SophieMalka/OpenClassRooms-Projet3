9e4c0ea068bf38d48d8f8cb287355423e8759198
SophieMalka <s.malka@live.fr>
2023-02-07 17:31:39 +0100

    Ouverture / fermeture modale ok

--- FrontEnd/js/modale.js
+++ FrontEnd/js/modale.js
@@ -7,7 +7,9 @@ const openModal = function (e) {
     target.removeAttribute('aria-hidden');
     target.setAttribute('aria-modal', 'true');
     modal = target;
-    modal.addEventListener('click', closeModal)
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-close-modal').addEventListener('click', closeModal);
+    modal.querySelector('.js-stop-modal').addEventListener('click', stopPropagation);
 };
 
 const closeModal = function (e) {
@@ -16,8 +18,14 @@ const closeModal = function (e) {
     modal.style.display = "none";
     modal.setAttribute('aria-hidden', 'true');
     modal.removeAttribute('aria-modal');
-    modal.removeEventListener('click', closeModal)
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.js-close-modal').removeEventListener('click', closeModal);
+    modal.querySelector('.js-stop-modal').removeEventListener('click', stopPropagation);
     modal = null;
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation()
 }
 
 document.querySelectorAll('.js-modal').forEach(a => {
@@ -26,67 +34,61 @@ document.querySelectorAll('.js-modal').forEach(a => {
 
 function displayWorksModal() {
     fetch("http://localhost:5678/api/works")
-    .then(function(response) {
-        console.log(response);
-        if(response.ok) {
-            return response.json();
-    }
-    })
+        .then(function (response) {
+            console.log(response);
+            if (response.ok) {
+                return response.json();
+            }
+        })
 
-    .then(function(data) {
-    // une fois qu'on a les données de l'API
-    console.log(data);
+        .then(function (data) {
+            // une fois qu'on a les données de l'API
+            console.log(data);
 
-    const modalGallery = document.getElementById('modal-gallery');
-    console.log(modalGallery);
+            for (let work of data) {
+                let newFigure = document.createElement("figure");
+                newFigure.style.width = '85px';
+                newFigure.style.height = '128px';
+                newFigure.style.position = 'relative';
 
-    //ajout des works issus de l'API (boucle sur les données de l'API)
-    //pour chaque donnée de l'API...
-    setWorksModal(data);
-    });
+                let newImg = document.createElement("img");
+                newImg.setAttribute("crossorigin", "anonymous");
+                newImg.setAttribute("src", work.imageUrl);
+                newImg.alt = work.title;
+                newImg.style.width = '100%';
+                newImg.style.objectFit = 'cover';
 
+                let deleteIcon = document.createElement("i");
+                deleteIcon.setAttribute("id", work.id);
+                deleteIcon.classList.add("fa-solid", "fa-trash-can");
+                deleteIcon.style.position = 'absolute';
+                deleteIcon.style.color = '#FFFFFF';
+                deleteIcon.style.fontSize = '14px';
+                deleteIcon.style.backgroundColor = '#000000';
+                deleteIcon.style.padding = '4px';
+                deleteIcon.style.borderRadius = '2px';
+                deleteIcon.style.marginLeft = '55px';
+                deleteIcon.style.marginTop = '6px';
+                deleteIcon.addEventListener('click', deleteWorksModal);
+
+
+                let newCaption = document.createElement("figcaption");
+                newCaption.innerText = 'éditer';
+
+                newFigure.appendChild(deleteIcon);
+                newFigure.appendChild(newImg);
+                newFigure.appendChild(newCaption);
+
+                const modalGallery = document.getElementById('modal-gallery');
+                console.log(modalGallery);
+
+                modalGallery.appendChild(newFigure);
+            }
+        })
 };
 
-function setWorksModal(data) {
-
-    for (let work of data) {
-        let newFigure = document.createElement("figure");
-        newFigure.style.width = '85px';
-        newFigure.style.height = '128px';
-        newFigure.style.position = 'relative';
-
-        let newImg = document.createElement("img");
-        newImg.setAttribute("crossorigin", "anonymous");
-        newImg.setAttribute("src", work.imageUrl);
-        newImg.alt = work.title;
-        newImg.style.width = '100%';
-        newImg.style.objectFit = 'cover';
-
-        let deleteIcon = document.createElement("i");
-        deleteIcon.classList.add("fa-solid", "fa-trash-can");
-        deleteIcon.style.position = 'absolute';
-        deleteIcon.style.color = '#FFFFFF';
-        deleteIcon.style.fontSize = '14px';
-        deleteIcon.style.backgroundColor = '#000000';
-        deleteIcon.style.padding = '4px';
-        deleteIcon.style.borderRadius = '2px';
-        deleteIcon.style.marginLeft = '55px';
-        deleteIcon.style.marginTop = '6px';
-
-
-        let newCaption = document.createElement("figcaption");
-        newCaption.innerText = 'éditer';
-
-        newFigure.appendChild(deleteIcon);
-        newFigure.appendChild(newImg);
-        newFigure.appendChild(newCaption);
-
-        const modalGallery = document.getElementById('modal-gallery');
-        console.log(modalGallery);
-
-        modalGallery.appendChild(newFigure);
-    }
-};
+function deleteWorksModal() {
+    
+}
 
 displayWorksModal();
-setWorksModal();
